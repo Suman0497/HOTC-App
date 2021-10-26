4f104565c28ff27638489ad24e63aa0922e845f9
Suman <sumanreshmj@gmail.com>
2021-10-26 16:21:50 +0530

    Initial Commit

---
+++ Wedding Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wedding Album
+//
+//  Created by apple on 26/10/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
